c522c9b5bbcd29736ad33bb511d8aa74da40133b
DavidWells <DavidWells@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-07 21:55:12 +0000

    Version 2 commit all new everything
    git-svn-id: https://plugins.svn.wordpress.org/cta/trunk@871464 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wordpress-cta.php
+++ wordpress-cta.php
@@ -3,30 +3,29 @@
 Plugin Name: Calls to Action
 Plugin URI: http://www.inboundnow.com/cta/
 Description: Display Targeted Calls to Action on your WordPress site.
-Version: 1.3.3
-Author: Inbound Now
+Version: 2.0.0
+Author: InboundNow
 Author URI: http://www.inboundnow.com/
 Text Domain: cta
 Domain Path: shared/languages/cta/
 */
 
 // DEFINE CONSTANTS AND GLOBAL VARIABLES
-define('WP_CTA_CURRENT_VERSION', '1.3.3' );
+define('WP_CTA_CURRENT_VERSION', '2.0.0' );
 define('WP_CTA_URLPATH', WP_PLUGIN_URL.'/'.plugin_basename( dirname(__FILE__) ).'/' );
 define('WP_CTA_PATH', WP_PLUGIN_DIR.'/'.plugin_basename( dirname(__FILE__) ).'/' );
 define('WP_CTA_SLUG', plugin_basename( dirname(__FILE__) ) );
 define('WP_CTA_FILE', __FILE__ );
-define('WP_CTA_TEXT_DOMAIN', 'cta' );
 
 $uploads = wp_upload_dir();
-define('WP_CTA_UPLOADS_PATH', $uploads['basedir'].'/wp-calls-to-action/templates/' );
-define('WP_CTA_UPLOADS_URLPATH', $uploads['baseurl'].'/wp-calls-to-action/templates/' );
+define('WP_CTA_UPLOADS_PATH', $uploads['basedir'].'/calls-to-action/templates/' );
+define('WP_CTA_UPLOADS_URLPATH', $uploads['baseurl'].'/calls-to-action/templates/' );
 define('WP_CTA_STORE_URL', 'http://www.inboundnow.com/cta/' );
 
 
-
-if (is_admin())
+if (is_admin()){
 	if(!isset($_SESSION)){@session_start();}
+}
 
 /* load core files */
 switch (is_admin()) :
@@ -43,6 +42,7 @@ switch (is_admin()) :
 		include_once('modules/module.install.php');
 		include_once('modules/module.extension-updater.php');
 		include_once('modules/module.ab-testing.php');
+		include_once('modules/module.widgets.php');
 		include_once('modules/module.calls-to-action.php');
 		include_once('modules/module.load-extensions.php');
 		include_once('modules/module.metaboxes-global.php');
@@ -52,19 +52,57 @@ switch (is_admin()) :
 		include_once('modules/module.utils.php');
 		include_once('modules/module.customizer.php');
 		include_once('modules/module.track.php');
-		include_once('modules/module.alert.php');
+
+		/* Temp update notice */
+		function wp_major_cta_update_notice(){
+		    global $pagenow;
+		    global $current_user ;
+		    $user_id = $current_user->ID;
+		    if ( ! get_user_meta($user_id, 'wp_cta_major_update_ignore') ) {
+		             echo '<div class="updated" style="position:relative;">
+		             <a style="position: absolute; font-size: 20px; top: 10px; right: 30px;" href="?wp_cta_major_update_ignore=0">
+		             Sounds good! Hide This Message
+		             </a>
+		             	<h1>Welcome to WordPress Calls to Action Version 2.0</h1>
+		             	<p>Please visit and re-save your current call to action templates. There were a number of necessary changes to the plugin to make it much much better!</p>
+		                 <p><b style="font-size:18px; font-weight:bold;margin-bottom:5px;">Whats new?</b><br>
+
+		                    <ul style="list-style: square; padding-left: 20px;margin-top: -10px;">
+			                    <li>Faster Call to Action load times</li>
+			                    <li>Better A/B Testing functionality</li>
+			                    <li>Mobile responsive Call to action templates</li>
+			                    <li>A new & improved call to action templating engine</li>
+			                    <li>All around code improvements</li>
+		                    </ul>
+						<b style="font-size:18px; font-weight:bold;margin-bottom:5px;margin-top:20px; display:block;">Whats different?</b>
+						We had to back out popups and slidein functionality for the initial release. Also, the like to download CTA is no longer in the plugin.<br><br>
+		                <span style="color:red;">Important:</span> We are no longer supporting versions lower than 2.0.0. You can revert to the older version of the CTA plugin by downloading the files here: http://wordpress.org/plugins/cta/developers/ under "Other Versions"
+		                 </p>
+		             </div>';
+		    }
+		}
+		add_action('admin_notices', 'wp_major_cta_update_notice');
+		add_action('admin_init', 'wp_cta_major_notice_ignore');
+		function wp_cta_major_notice_ignore() {
+		    global $current_user;
+		        $user_id = $current_user->ID;
+		        if ( isset($_GET['wp_cta_major_update_ignore']) && '0' == $_GET['wp_cta_major_update_ignore'] ) {
+		             add_user_meta($user_id, 'wp_cta_major_update_ignore', 'true', true);
+		    }
+		}
 
 		BREAK;
 
 	case false :
 		/* load front-end files */
 		// include_once('functions/functions.global.php'); // old
+		include_once('modules/module.load-extensions.php');
 		include_once('modules/module.post-type.php');
 		include_once('modules/module.enqueue.php');
 		include_once('modules/module.track.php');
 		include_once('modules/module.click-tracking.php');
 		include_once('modules/module.ajax-setup.php');
-
+		include_once('modules/module.widgets.php');
 		include_once('modules/module.cookies.php');
 		include_once('modules/module.ab-testing.php');
 		include_once('modules/module.calls-to-action.php');
@@ -73,36 +111,6 @@ switch (is_admin()) :
 
 		BREAK;
 endswitch;
-
-include_once('modules/module.widgets.php'); // Loads in both
-
-/* TEMP function to make ajax call function */
-// Set Leads to list from form tool. Need to consolidate into add_lead_to_list_tax
-if (!function_exists('add_lead_lists_ajax')) {
-function add_lead_lists_ajax($lead_id, $list_id, $tax = 'wplead_list_category') {
-
-    $current_lists = wp_get_post_terms( $lead_id, $tax, 'id' );
-    $all_term_ids = array();
-    $all_term_slugs = array();
-    foreach ($current_lists as $term ) {
-        $add = $term->term_id;
-        $slug = $term->slug;
-        $all_term_ids[] = $add;
-        $all_term_slugs[] = $slug;
-    }
-    // Set terms for lead tags taxomony
-    $list_array = $list_id;
-    if(is_array($list_array)) {
-        foreach ($list_array as $key => $value) {
-            $num = intval($value);
-            if ( !in_array($num, $all_term_ids) ) {
-                $all_term_ids[] = $num;
-                wp_set_object_terms( $lead_id, $all_term_ids, $tax);
-            }
-        }
-    }
-}
-}
 
 /* Inbound Core Shared Files. */
 add_action( 'plugins_loaded', 'inbound_load_shared' , 12);
@@ -115,29 +123,24 @@ function inbound_load_shared(){
 	/* Define Shared Constant for Load Prevention*/
 	define('INBOUDNOW_SHARED','loaded');
 
-	include_once('shared/tracking/store.lead.php'); // Lead Storage from cta
+	include_once('shared/tracking/store.lead.php'); // Lead Storage from landing pages
 	include_once('shared/classes/form.class.php');  // Mirrored forms
-	include_once('shared/classes/debug.class.php');  // Inbound Debug & Scripts Class
-	include_once('shared/classes/compatibility.class.php');  // Inbound Compatibility Class
-	include_once('shared/inboundnow/inboundnow.extend.php'); // Legacy
-	include_once('shared/inboundnow/inboundnow.extension-licensing.php'); // Inboundnow Package Licensing
-	include_once('shared/inboundnow/inboundnow.extension-updating.php'); // Inboundnow Package Updating
-	include_once('shared/inboundnow/inboundnow.global-settings.php'); // Inboundnow Global Settings
-	include_once('shared/metaboxes/template.metaboxes.php');  // Shared Shortcodes
-	include_once('shared/inbound-shortcodes/inbound-shortcodes.php');  // Shared Shortcodes
 	include_once('shared/classes/menu.class.php');  // Inbound Marketing Menu
 	include_once('shared/classes/feedback.class.php');  // Inbound Feedback Form
+	include_once('shared/classes/debug.class.php');  // Inbound Debug & Scripts Class
+	include_once('shared/classes/compatibility.class.php');  // Inbound Compatibility Class
+	include_once('shared/inbound-shortcodes/inbound-shortcodes.php');  // Shared Shortcodes
+	include_once('shared/extend/inboundnow.extend.php');
+	include_once('shared/extend/inboundnow.extension-licensing.php'); // Legacy - Inboundnow Package Licensing
+	include_once('shared/extend/inboundnow.extension-updating.php'); // Legacy -Inboundnow Package Updating
+	include_once('shared/extend/inboundnow.global-settings.php'); // Inboundnow Global Settings
+	include_once('shared/metaboxes/template.metaboxes.php');  // Shared Shortcodes
+	include_once('shared/functions/global.shared.functions.php'); // Global Shared Utility functions
+	include_once('shared/assets/assets.loader.class.php');  // Load Shared CSS and JS Assets
+	include_once('shared/functions/global.leads.cpt.php'); // Shared Lead functionality
+
 }
 
-/* Display Where CTA shortcodes are being used
-$test = show_pages_containing_ctas('cta id=\"1802\"');
-print_r($test);
 
-function show_pages_containing_ctas($tag,$limit=1){
-	global $wpdb;
-	$sql = "SELECT ID FROM `{$wpdb->posts}` WHERE post_content LIKE \"%[$tag%\" LIMIT $limit";
-	$sql = "SELECT DISTINCT(ID) FROM `{$wpdb->posts}` WHERE post_content LIKE \"%[$tag%\" AND post_status IN ('publish') LIMIT $limit";
-	$ids = $wpdb->get_col($sql,0);
-	return (is_array($ids)&&count($ids)>0)?$ids:array();
-}
-*/
+
+?>
