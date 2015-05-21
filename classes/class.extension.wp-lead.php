6d9b55429281ab6553512cd36f336b7b90020a8b
DavidWells <DavidWells@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-21 22:34:39 +0000

    security patch
    git-svn-id: https://plugins.svn.wordpress.org/cta/trunk@1165282 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/class.extension.wp-lead.php
+++ classes/class.extension.wp-lead.php
@@ -9,22 +9,25 @@ if ( !class_exists('CTA_WordPress_Leads') ) {
 		}
 
 		private function load_hooks() {
-			
+
 			add_filter('wpl_lead_activity_tabs', array( __CLASS__ , 'create_nav_tabs' ) , 10, 1);
 			add_action('wpleads_after_activity_log' , array( __CLASS__ , 'show_cta_click_content' ) );
-			
+
 		}
-		
+
 		/* Create New Nav Tabs in WordPress Leads - Lead UI */
-		public static function create_nav_tabs( $nav_items )
-		{
-			$nav_items[] = array('id'=>'wpleads_lead_cta_click_tab','label'=> __( 'CTA Clicks' , 'cta' ) );
+		public static function create_nav_tabs( $nav_items ) {
+			$nav_items[] = array(
+				'id'=>'wpleads_lead_cta_click_tab',
+				'label'=> __( 'CTA Clicks' , 'cta' ),
+				'count' => self::get_click_count()
+			);
 			return $nav_items;
 		}
-		
+
 		/* Display CTA Click Content */
 		public static function show_cta_click_content() {
-			global $post; 
+			global $post;
 			?>
 			<div id="wpleads_lead_cta_click_tab" class='lead-activity'>
 				<h2><?php _e( 'CTA\'s Clicked' , 'cta' ); ?></h2>
@@ -37,20 +40,18 @@ if ( !class_exists('CTA_WordPress_Leads') ) {
 
 				$the_array = json_decode($events, true);
 				// echo "First id : ". $the_array[1]['id'] . "!"; // Get specific value
-				if ($events)
-				{
+				if ($events) {
 					$count = 1;
 
-					foreach($the_array as	$key=>$val)
-					{
+					foreach($the_array as $key=>$val) {
 						$id = $the_array[$count]['id'];
 						$title = get_the_title($id);
-	
+
 						$date_raw = new DateTime($the_array[$count]['datetime']);
-	
+
 						$date_of_conversion = $date_raw->format('F jS, Y \a\t g:ia (l)');
 						$clean_date = $date_raw->format('Y-m-d H:i:s');
-					
+
 						echo '<div class="lead-timeline recent-conversion-item cta-tracking-item" data-date="'.$clean_date.'">
 									<a class="lead-timeline-img" href="#non">
 										<!--<i class="lead-icon-target"></i>-->
@@ -77,8 +78,24 @@ if ( !class_exists('CTA_WordPress_Leads') ) {
 			<?php
 		}
 
+
+		/**
+		*	Gets number of tracked link clicks
+		*/
+		public static function get_click_count() {
+			global $post;
+
+			$clicks = get_post_meta( $post->ID , 'call_to_action_clicks' , true);
+			$clicks = json_decode( $clicks , true);
+
+			if ( isset($clicks) && is_array($clicks) ) {
+				return count($clicks);
+			} else {
+				return 0;
+			}
+		}
 	}
-	
+
 	/* Load Post Type Pre Init */
 	$GLOBALS['CTA_WordPress_Leads'] = new CTA_WordPress_Leads();
 }
